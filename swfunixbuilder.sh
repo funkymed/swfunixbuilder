47c1e8b644883aa6e99306c9044adf8be6f6d606
Cyril <cyril.pereira@gmail.com>
2013-07-16 20:47:43 +0200

    code indent

--- swfunixbuilder.sh
+++ swfunixbuilder.sh
@@ -31,64 +31,64 @@ then
 
 	case "$command" in
 		  "compile")
-		TXT="Generate .ipa file in ipa-ad-hoc (slow)"
-		_title ${TXT}
-		echo "Please wait ... compiling..."
-		echo $PWD/$FILENAME_PROD
-		"$PATH_ADT" -package -target ipa-ad-hoc -provisioning-profile $PATH_PROVISION -keystore $PATH_KEY -storetype PKCS12 -storepass 			 $PASSWORD $FILENAME_PROD $FILENAME_MAIN-app.xml $FILENAME_MAIN.swf
-		echo "done."    
-		exit 1;          
+			TXT="Generate .ipa file in ipa-ad-hoc (slow)"
+			_title ${TXT}
+			echo "Please wait ... compiling..."
+			echo $PWD/$FILENAME_PROD
+			"$PATH_ADT" -package -target ipa-ad-hoc -provisioning-profile $PATH_PROVISION -keystore $PATH_KEY -storetype PKCS12 -storepass $PASSWORD $FILENAME_PROD $FILENAME_MAIN-app.xml $FILENAME_MAIN.swf
+			echo "done."    
+			exit 1;          
 		;;
 		  "fastcompile")
-		TXT="Generate .ipa file in ipa-test-interpreter (fast)"
-		_title ${TXT}
-		echo "Please wait ... compiling..."
-		echo $PWD/$FILENAME_PROD
-		"$PATH_ADT" -package -target ipa-test-interpreter -provisioning-profile $PATH_PROVISION -keystore $PATH_KEY -storetype PKCS12 -storepass 			 $PASSWORD $FILENAME_PROD $FILENAME_MAIN-app.xml $FILENAME_MAIN.swf
-		echo "done."    
+			TXT="Generate .ipa file in ipa-test-interpreter (fast)"
+			_title ${TXT}
+			echo "Please wait ... compiling..."
+			echo $PWD/$FILENAME_PROD
+			"$PATH_ADT" -package -target ipa-test-interpreter -provisioning-profile $PATH_PROVISION -keystore $PATH_KEY -storetype PKCS12 -storepass $PASSWORD $FILENAME_PROD $FILENAME_MAIN-app.xml $FILENAME_MAIN.swf
+			echo "done."    
 		exit 1; 
 		;;        
 		"deploy")             
-		TXT="Deploy test version on device"
-		_title ${TXT}
-		echo "Please wait ... sending ..."        
-		echo $PWD/$FILENAME_PROD          
-		"$PATH_TRANSPORT" -v $FILENAME_PROD
-		echo "done."
-		exit 1;       
+			TXT="Deploy test version on device"
+			_title ${TXT}
+			echo "Please wait ... sending ..."        
+			echo $PWD/$FILENAME_PROD          
+			"$PATH_TRANSPORT" -v $FILENAME_PROD
+			echo "done."
+			exit 1;       
 		;;     
 		"simulate")
-		TXT="Generate .ipa for simulator"
-		_title ${TXT}
-		echo "please wait .... compiling ..."   
-		echo $PWD/$FILENAME_TEST    
-		"$PATH_ADT" -package -target ipa-test-interpreter-simulator -provisioning-profile $PATH_PROVISION -keystore $PATH_KEY -storetype PKCS12 -storepass $PASSWORD $FILENAME_TEST $FILENAME_MAIN-app.xml $FILENAME_MAIN.swf -platformsdk $PATH_SDK
-		echo "done" 
-		exit 1;     
+			TXT="Generate .ipa for simulator"
+			_title ${TXT}
+			echo "please wait .... compiling ..."   
+			echo $PWD/$FILENAME_TEST    
+			"$PATH_ADT" -package -target ipa-test-interpreter-simulator -provisioning-profile $PATH_PROVISION -keystore $PATH_KEY -storetype PKCS12 -storepass $PASSWORD $FILENAME_TEST $FILENAME_MAIN-app.xml $FILENAME_MAIN.swf -platformsdk $PATH_SDK
+			echo "done" 
+			exit 1;     
 		;;          
 		"launch")
-		TXT="Install & load simulation" 
-		_title ${TXT}
-		echo "please wait ... installing ..." 
-		echo $PWD/$FILENAME_TEST
-		"$PATH_ADT" -installApp -platform ios -platformsdk $PATH_SDK -device ios-simulator -package $FILENAME_TEST
-		echo "please wait ... loading ..."
-		"$PATH_ADT" -launchApp -platform ios -platformsdk $PATH_SDK -device ios-simulator -appid $ID
-		echo "done."
-		exit 1;     
+			TXT="Install & load simulation" 
+			_title ${TXT}
+			echo "please wait ... installing ..." 
+			echo $PWD/$FILENAME_TEST
+			"$PATH_ADT" -installApp -platform ios -platformsdk $PATH_SDK -device ios-simulator -package $FILENAME_TEST
+			echo "please wait ... loading ..."
+			"$PATH_ADT" -launchApp -platform ios -platformsdk $PATH_SDK -device ios-simulator -appid $ID
+			echo "done."
+			exit 1;     
 		;;
 		"run")
-		./swfunixbuilder.sh simulate
-		./swfunixbuilder.sh launch
+			./swfunixbuilder.sh simulate
+			./swfunixbuilder.sh launch
 		;;
 		"help")
-		_help
-		exit 1;     
+			_help
+			exit 1;     
 		;;          
 		"credit")
-		echo "Swf unix builder (2013-07)"
-		echo "Shell code by Cyril Pereira, Ruby by Igor Sokolov"
-		exit 1;     
+			echo "Swf unix builder (2013-07)"
+			echo "Shell code by Cyril Pereira, Ruby by Igor Sokolov"
+			exit 1;     
 		;;
 	esac
 	_help                                                          
