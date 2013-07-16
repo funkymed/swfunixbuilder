31a16a84549de67c41a4a8320518fbf2009b0117
Cyril <cyril.pereira@gmail.com>
2013-07-16 16:35:39 +0200

    instruction

--- compile.sh
+++ compile.sh
@@ -5,5 +5,5 @@ echo "*                        making device version                            
 echo "***************************************************************************"
 echo "Please wait ... compiling..."
 echo $PWD/$FILENAME_PROD
-echo "$PATH_ADT" -package -target ipa-test-interpreter -provisioning-profile $PATH_PROVISION -keystore $PATH_KEY -storetype PKCS12 -storepass $PASSWORD $FILENAME_PROD $FILENAME_MAIN-app.xml $FILENAME_MAIN.swf
+"$PATH_ADT" -package -target ipa-test-interpreter -provisioning-profile $PATH_PROVISION -keystore $PATH_KEY -storetype PKCS12 -storepass $PASSWORD $FILENAME_PROD $FILENAME_MAIN-app.xml $FILENAME_MAIN.swf
 echo "done."                      
